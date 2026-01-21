f3ac61a2c82fd3fb7c1a61be93421a4cd2efc8e0
= <=>
2026-01-21 02:59:54 +0000

    validate register by zod

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 const router = express.Router();
 import { register, login, logout } from "./../controllers/authController.js";
-router.post("/register", register);
-router.post("/login", login);
+import {
+  validateLoginSchema,
+  validateRegisterSchema,
+} from "../validators/validateAuthSchema.js";
+import validateRequest from "../middlewares/validateRequest.js";
+router.post("/register", validateRequest(validateRegisterSchema), register);
+router.post("/login", validateRequest(validateLoginSchema), login);
 router.get("/logout", logout);
 
 export default router;
--- src/validators/validateAuthSchema.js
+++ src/validators/validateAuthSchema.js
@@ -1,11 +1,20 @@
 import { email, z } from "zod";
-const validateLoginSchema = z.object({
+export const validateLoginSchema = z.object({
+  email: z.email(),
+  password: z
+    .string()
+    .min(5, "minimum length for password is 5 characters")
+    .nonempty(),
+});
+export const validateRegisterSchema = z.object({
+  name: z
+    .string()
+    .nonempty()
+    .min(3, "min length for name is 5 chars")
+    .max(20, "name is very large"),
   email: z.email(),
   password: z
     .string()
     .nonempty()
-    .nonoptional()
-    .minLength(5, "minimum length for password is 5 characters"),
+    .min(5, "minimum length for password is 5 characters"),
 });
-
-export default validateLoginSchema;
